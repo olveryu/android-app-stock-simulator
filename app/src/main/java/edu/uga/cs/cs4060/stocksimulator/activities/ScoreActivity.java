1f7a9a9d3f6acd8458f14d2bb58cf93d5c366fc2
zhenyu yan <469022042@qq.com>
2018-12-02 18:09:29 -0500

    leaderBoard activity

---
+++ app/src/main/java/edu/uga/cs/cs4060/stocksimulator/activities/ScoreActivity.java
@@ -0,0 +1,13 @@
+package edu.uga.cs.cs4060.stocksimulator.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+}
